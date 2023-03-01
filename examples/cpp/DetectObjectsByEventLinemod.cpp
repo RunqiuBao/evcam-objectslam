eeee5f32ae98f637326b93b940df74678bd5440f
Runqiu Bao <runqiu.bao@tum.de>
2023-03-02 00:27:51 +0900

    execute python in cpp main program

--- examples/cpp/DetectObjectsByEventLinemod.cpp
+++ examples/cpp/DetectObjectsByEventLinemod.cpp
@@ -1,3 +1,5 @@
+#include <Python.h>
+
 #include <logging.h>
 TDO_LOGGER("examples.DetectObjectsByEventLinemod")
 
@@ -8,5 +10,10 @@ int main(int argc, char* argv[]){
 
     TDO_LOG_DEBUG("Hellom, world!");
     TDO_LOG_DEBUG_FORMAT("shallom! %s", "aloha");
+
+    Py_Initialize();
+    PyRun_SimpleString("from time import time, ctime\n"
+                        "print('Today is', ctime(time()))\n");
+    Py_Finalize();
     return 0;
 }
