20d5847bfd1d69dc854eb68bbd3ea0e8913d5dec
Runqiu Bao <runqiu.bao@tum.de>
2023-03-07 00:44:59 +0900

    embed python dataloader in c++ executable

--- examples/cpp/DetectObjectsByEventLinemod.cpp
+++ examples/cpp/DetectObjectsByEventLinemod.cpp
@@ -12,15 +12,72 @@ int main(int argc, char** argv){
      *    - eventdatabaseformat: format of the event database
      *    - templatepath: path to the template images
      **/
-    ArgumentParser argparser(argc, argv);
+    std::vector<std::string> options{
+        "inputdata",
+        "eventdatabaseformat",
+        "templatepath"
+    };
+    ArgumentParser argparser(argc, argv, options);
     // log4cxx::BasicConfigurator::configure();
     tooldetectobject::ConfigureRootLogger("DEBUG", "", "./detector.log");
 
     TDO_LOG_DEBUG("-------- start of the detector! --------");
+    PyObject *myModuleName, *myModule, *myDict, *myPythonClass, *myPythonObject, *dataPathPy;
 
     Py_Initialize();
-    // PyRun_SimpleString("from time import time, ctime\n"
-    //                     "print('Today is', ctime(time()))\n");
+    myModuleName = PyUnicode_FromString(
+        "tool_eventdetectobjects.io.file_format.eventsinethzformat"
+    );
+    dataPathPy = PyUnicode_FromString(
+        argparser.getCmdOption("inputdata").c_str()
+    );
+
+    // load the module object
+    myModule = PyImport_Import(myModuleName);
+    if (myModule == nullptr){
+        PyErr_Print();
+        TDO_LOG_ERROR("Failed to import the python module.");
+        return 0;
+    }
+    // Py_DECREF(myModuleName);
+
+    // load the classes into a dict
+    myDict = PyModule_GetDict(myModule);
+    if (myDict == nullptr){
+        PyErr_Print();
+        TDO_LOG_ERROR("Failed to get the dictionary.");
+        return 0;
+    }
+    // Py_DECREF(myModule);
+
+    // build the name of a callable class
+    myPythonClass = PyDict_GetItemString(myDict, "EventsInETHZFormat");
+    if (myPythonClass == nullptr){
+        PyErr_Print();
+        TDO_LOG_ERROR("Failed to get the python class.");
+        return 0;
+    }
+    // Py_DECREF(myDict);
+
+    // create an instance of the class
+    if (PyCallable_Check(myPythonClass)){
+        myPythonObject = PyObject_CallObject(myPythonClass, dataPathPy);
+        // Py_DECREF(myPythonClass);
+    } else {
+        TDO_LOG_ERROR("Can not instantiate the python class");
+        // Py_DECREF(myPythonClass);
+        return 0;
+    }
+
+    PyObject *functionAName;
+    functionAName = PyUnicode_FromString(
+        "PopOneTimeLimitedSbn"
+    );
+    for (int indexFrame=0; indexFrame < 100; indexFrame++){
+        PyObject* mysbn = PyObject_CallMethodObjArgs(myPythonObject, functionAName, 20000, 720, 1280);
+
+    }
+
     Py_Finalize();
     return 0;
 }
