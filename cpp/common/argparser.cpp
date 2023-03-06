73e5406dd44d0c2a286a9cfe3a239462e89200b5
Runqiu Bao <runqiu.bao@tum.de>
2023-03-07 00:45:38 +0900

    finish c++ argparser class

---
+++ cpp/common/argparser.cpp
@@ -0,0 +1,30 @@
+#include "argparser.h"
+#include "logging.h"
+TDO_LOGGER("tdo.common.argparser")
+
+
+ArgumentParser::ArgumentParser(int& argc, char** argv, const std::vector<std::string> optionLabels){
+    TDO_LOG_INFO("Arguments: \n");
+    for (int i=1; i < argc; i++){
+        TDO_LOG_INFO_FORMAT("%*%s: %s", 4%optionLabels[i]%std::string(argv[i]));
+        this->tokens.push_back(std::string(argv[i]));
+    }
+}
+
+const std::string& ArgumentParser::getCmdOption(const std::string& option) const{  // const at the end means this function does not change object states
+    std::vector<std::string>::const_iterator itr;
+    itr = std::find(this->tokens.begin(), this->tokens.end(), option);
+    if (itr != this->tokens.end() && ++itr != this->tokens.end()){
+        return *itr;
+    }
+    static const std::string emptyString("");
+    return emptyString;
+}
+
+const int ArgumentParser::getCmdOptionIndex(const std::string& option) const{
+    std::vector<std::string>::const_iterator itr;
+    itr = std::find(this->tokens.begin(), this->tokens.end(), option);
+    if (itr == this->tokens.end())
+        return -1;
+    return std::distance(this->tokens.begin(), itr);
+}
--- cpp/common/argparser.h
+++ cpp/common/argparser.h
@@ -6,32 +6,15 @@
 #include <string>
 #include <algorithm>
 
+
 class ArgumentParser {
 
 public:
-    ArgumentParser (int& argc, char** argv){
-        for (int i=1; i < argc; i++){
-            this->tokens.push_back(std::string(argv[i]));
-        }
-    }
+    ArgumentParser(int& argc, char** argv, const std::vector<std::string> optionLabels);
 
-    const std::string& getCmdOption(const std::string& option) const{  // const at the end means this function does not change object states
-        std::vector<std::string>::const_iterator itr;
-        itr = std::find(this->tokens.begin(), this->tokens.end(), option);
-        if (itr != this->tokens.end() && ++itr != this->tokens.end()){
-            return *itr;
-        }
-        static const std::string emptyString("");
-        return emptyString;
-    }
+    const std::string& getCmdOption(const std::string& option) const;
 
-    const int getCmdOptionIndex(const std::string& option) const{
-        std::vector<std::string>::const_iterator itr;
-        itr = std::find(this->tokens.begin(), this->tokens.end(), option);
-        if (itr == this->tokens.end())
-            return -1;
-        return std::distance(this->tokens.begin(), itr);
-    }
+    const int getCmdOptionIndex(const std::string& option) const;
 
     bool cmdOptionExists(const std::string &option) const {
         return std::find(this->tokens.begin(), this->tokens.end(), option) != this->tokens.end();
