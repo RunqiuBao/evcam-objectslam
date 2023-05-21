4332ebc3b320b1c28a3e2e2c12317d326f105c1d
Runqiu Bao <runqiu.bao@tum.de>
2023-05-21 18:44:11 +0900

    use new layout

---
+++ citation.md
@@ -0,0 +1,6 @@
+---
+layout: default
+permalink: /citation/
+---
+
+# Citation
--- src/article.js
+++
@@ -1,9 +0,0 @@
-const article = `# Markdown-Navbar Demo
-
-The current page is a demo for markdown-navbar You can click **the button below** to edit or debug it on *codesandbox.io*.
-
-[![Edit markdown-navbar-demo-online](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/markdown-navbar-demo-online-e7e0n?fontsize=14&hidenavigation=1&theme=dark)
-
-## Chapter 1
-
-Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken Chicken.
