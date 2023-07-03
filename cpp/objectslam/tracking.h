836e4b89fbf6961360d2bab2b2a01f60959eac18
Runqiu Bao <runqiu.bao@tum.de>
2023-07-03 21:54:33 +0900

    create object slam module

---
+++ cpp/objectslam/tracking.h
@@ -0,0 +1,21 @@
+namespace eventobjectslam{
+
+// tracker state
+enum class Tracker_Status {
+    NotInitialized,
+    Initializing,
+    Tracking,
+    Lost
+};
+
+
+class Tracker {
+
+public:
+    // constructor
+    Tracker(system* system, )
+
+};
+
+
+}
